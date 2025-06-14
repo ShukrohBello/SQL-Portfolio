d8f9c1c78bd64ecbadea9188b8b0a89763974855
ShukrohBello <adetejubello@gmail.com>
2025-06-14 18:56:02 +0100

    sql mastery

---
+++ 8-Week-SQL-Challenge/Case Study #1 - Danny's Diner/DannysDinerSchema.sql
@@ -0,0 +1,7 @@
+----------------------------------
+-- CASE STUDY #1: DANNY'S DINER --
+----------------------------------
+
+-- Author: Shukurat Bello
+-- Date: 14/06/2025
+-- Tool used: Microsoft SQL Server
